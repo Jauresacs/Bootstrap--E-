f91952dacc4159a05ee9cba5f942637930ead9f0
Chloé Belard <ketchemone@hotmail.fr>
2025-10-06 16:02:03 +0200

    Ajout tables models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# Table/Modèle User
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10)
+
+# Table/modèle Team
+class Team(models.Model):
+    team_name = models.CharField(max_length=30)
+    team_description = models.CharField(max_length=255)
+    team_members = models.ManyToManyField(User)
+    #team_manager = models.ForeignKey(User, on_delete=models.CASCADE)
